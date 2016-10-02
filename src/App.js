c53353d26a7909f921b854b4a36fbfe766ed696d
Alan Tweedie <aetweedie@gmail.com>
2016-10-02 15:57:43 -0600

    Initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Coin Collector</h2>
+          <h3>Manage and sell your coins online</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
